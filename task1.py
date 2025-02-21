1d127c645bd455c35185678cfb30c0d6739a04b1
Anastasia_Muntyaeva <nastya2014myn@yandex.ru>
2025-02-21 21:59:48 +0300

    init: initional commit

---
+++ task1.py
@@ -0,0 +1,22 @@
+# todo: Определить в коде переменные:
+# 1. Целочисленного типа
+# 2. Вещественного типа
+# 3. Логического типа
+# 4. Строкового типа
+# 5. Пустого типа
+# Вывести их типы.
+
+first_number = 23
+print(type(first_number))
+
+second_floatnumber = 0.6
+print(type(second_floatnumber))
+
+third_logic = True
+print(type(third_logic))
+
+fourth_string = "Кошка"
+print(type(fourth_string))
+
+fifth_none = None
+print(type(fifth_none))
